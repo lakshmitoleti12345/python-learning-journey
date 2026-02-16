9f9ce5caa008012d20183a37806524f273079c48
SatyaLakshmi Toleti <lakshmitoleti12345@gmail.com>
2026-02-16 18:56:55 +0530

    Add files via upload

---
+++ day1/simple_cal.py
@@ -0,0 +1,15 @@
+number1=float(input("enter num1="))
+number2=float(input("entter num2="))
+print("1-addition 2-subtraction 3-multipliction 4-division")
+options=input("enter options 1/2/3/4:")
+if options=='1':
+    print(number1+number2)
+elif options=='2':
+    print(number1-number2)
+elif options=='3':
+    print(number1*number2)
+elif options==4:
+    print(number1/number2)
+
+
+
---
+++ day1/swap2num.py
@@ -0,0 +1,9 @@
+num1=int(input("enter number 1:"))
+num2=int(input("enter number 2:"))
+print("before swap number1=",num1)
+print("before swap number2=",num2)
+temp=num1
+num1=num2
+num2=temp
+print("after swap number1=",num1)
+print(" after swap number2=",num2)
