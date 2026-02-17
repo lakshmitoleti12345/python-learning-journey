bcbe05f15a70bcdfbf94bf53661346295f944c6a
SatyaLakshmi Toleti <lakshmitoleti12345@gmail.com>
2026-02-17 19:22:29 +0530

    Add files via upload

---
+++ day2/factorial.py
@@ -0,0 +1,6 @@
+#factorial
+n=3
+result=1
+for i in range(1,n+1):
+    result=result*i
+print(result)
---
+++ day2/multiplication_table.py
@@ -0,0 +1,6 @@
+#multiplication table
+n=9
+k=10
+for i in range(1,k+1):
+    print(i,"*",n,"=",i*n)
+    
